6a9fe1f90e7cdd1a7004c136dd87118149958bc3
Lucas Ferreira <lucas.182.ferreira@hotmail.com>
2020-03-31 16:26:56 -0300

    Front end added

---
+++ plain-img-api/src/routes.js
@@ -0,0 +1,36 @@
+const routes = require('express').Router();
+const multer = require('multer');
+const multerConfig = require('./config/multer');
+
+const Post = require('./models/Post');
+
+routes.get('/post', async (req, res) => {
+  const posts = await Post.find();
+
+  return res.json(posts);
+});
+
+routes.post('/post', multer(multerConfig).single('file'), async (req, res) => {
+  const { originalname: name, size, key, location: url = '' } = req.file;
+  const post = await Post.create({
+    name,
+    size,
+    key,
+    url
+  });
+
+  return res.json(post);
+});
+
+routes.delete('/post/:id', async (req, res) => {
+  const post = await Post.findById(req.params.id);
+  await post.remove();
+  return res.send();
+});
+
+routes.delete('/all', async (req, res) => {
+  await Post.remove({});
+  return res.send();
+});
+
+module.exports = routes;
